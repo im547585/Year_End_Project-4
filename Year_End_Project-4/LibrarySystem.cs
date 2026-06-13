eb4347a456a22e153f3dfbdee0632386e4ea687c
Инихан И. Местан <im54758501@edu.mon.bg>
2026-06-13 16:48:52 +0300

    13.06.2026 г.; 17:00

--- Year_End_Project-4/LibrarySystem.cs
+++ Year_End_Project-4/LibrarySystem.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Year_End_Project_4
 {
     internal class LibrarySystem
     {
+        private string book_name;
+        private string author;
+        private string publishing_house;
+        private int year_of_publishing;
+        private int no_from_catalogue;
+        private string genre;
+        private int no_of_personal_card;
+        private string names_of_reader;
+        private long egn;
+        private string adress;
+        private string phone_number;
+        private int age_of_reader;
+        private string date_of_creating_personal_card;
+        private string date_of_returning_the_book;
+        private string no_of_returned_book_from_catalogue;
+        private string author_of_returned_book;
+        private string name_of_returned_book;
     }
 }

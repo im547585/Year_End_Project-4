5fc24dbe095565d1ec00183c33773aac6a6ba116
Инихан И. Местан <im54758501@edu.mon.bg>
2026-06-13 16:30:10 +0300

    Add project files.

---
+++ Year_End_Project-4/Ex.4_YEP.cs
@@ -0,0 +1,18 @@
+﻿namespace Year_End_Project_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравейте, моля изберете една от следните опции: \n\t" +
+                "1. Добавяне на книга/и;\n\t" +
+                "2. Търсене на книга;\n\t"+
+                "3. Редактиране на книга/и;\n\t" +
+                "4. Изтриване на книга/и; \n\t" +
+                "5. Сортиране на книга/и.");
+            Console.Write("ИЗБОР: ");
+            Console.WriteLine();
+            int Choice = int.Parse(Console.ReadLine());
+        }
+    }
+}

587525ea0db82d91a06cc1b1e930817f802b40fd
Инихан И. Местан <im54758501@edu.mon.bg>
2026-06-14 11:51:52 +0300

    .

---
+++ Year_End_Project-4/LibrarySystem_Books.cs
@@ -0,0 +1,149 @@
+﻿namespace Year_End_Project_4
+{
+    internal class LibrarySystem_Books
+    {
+        private string book_name; //**
+        private string author; //**
+        private string publishing_house;//**
+        private int year_of_publishing; //**
+        private int no_of_book_from_catalogue; //**
+        private string genre; //**
+        
+
+        public string Book_Name
+        {
+            get { return book_name; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    book_name = value;
+                }
+            }
+        }
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    author = value;
+                }
+            }
+        }
+        public string Publishing_House
+        {
+            get { return publishing_house; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    publishing_house = value;
+                }
+            }
+        }
+        public int Year_Of_Publishing
+        {
+            get { return year_of_publishing; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
+                    Console.ResetColor();
+                }
+                else if (value < 999 || value > 9999)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Годината ба издаване трябва да е 4 цифрено число!<<<!!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    year_of_publishing = value;
+                }
+            }
+        }
+        public int No_Of_Book_From_Catalogue
+        {
+            get { return no_of_book_from_catalogue; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
+                    Console.ResetColor();
+                }
+                else if (value < 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>№ от каталога трябва да е валиден!<<<!!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    no_of_book_from_catalogue = value;
+                }
+            }
+        }
+        public string Genre { get { return genre; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    genre = value;
+                }
+            }
+        }
+
+        public LibrarySystem_Books(string book_name, string author, string publishing_house, int year_of_publishing, int no_of_book_from_catalogue, string genre)
+        {
+            this.Book_Name = book_name;
+            this.Author = author;
+            this.Publishing_House = publishing_house;
+            this.Year_Of_Publishing = year_of_publishing;
+            this.No_Of_Book_From_Catalogue = no_of_book_from_catalogue;
+            this.Genre = genre;
+        } 
+        //private int AverageAgeOfBooks(int age)
+        //{
+        //    age = 2026 - year_of_publishing;
+        //    return age;
+        //}
+    }
+}
--- Year_End_Project-4/LibrarySystem_Reader.cs
+++ Year_End_Project-4/LibrarySystem_Reader.cs
@@ -1,13 +1,11 @@
-﻿namespace Year_End_Project_4
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Year_End_Project_4
 {
-    internal class LibrarySystem
+    internal class LibrarySystem_Reader
     {
-        private string book_name; //**
-        private string author; //**
-        private string publishing_house;//**
-        private int year_of_publishing; //**
-        private int no_of_book_from_catalogue; //**
-        private string genre; //**
         private int no_of_personal_card;//**
         private string names_of_reader; //**
         private long egn; //**
@@ -20,127 +18,9 @@
         private string author_of_returned_book; //**
         private string name_of_returned_book; //**
 
-        public string Book_Name
+        public int No_Of_Personal_Card
         {
-            get { return book_name; }
-            set
-            {
-                if (value == null)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    book_name = value;
-                }
-            }
-        }
-        public string Author
-        {
-            get { return author; }
-            set
-            {
-                if (value == null)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    author = value;
-                }
-            }
-        }
-        public string Publishing_House
-        {
-            get { return publishing_house; }
-            set
-            {
-                if (value == null)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    publishing_house = value;
-                }
-            }
-        }
-        public int Year_Of_Publishing
-        {
-            get { return year_of_publishing; }
-            set
-            {
-                if (value == null)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
-                    Console.ResetColor();
-                }
-                else if (value < 999 || value > 9999)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Годината ба издаване трябва да е 4 цифрено число!<<<!!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    year_of_publishing = value;
-                }
-            }
-        }
-        public int No_Of_Book_From_Catalogue
-        {
-            get { return no_of_book_from_catalogue; }
-            set
-            {
-                if (value == null)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
-                    Console.ResetColor();
-                }
-                else if (value < 0)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>№ от каталога трябва да е валиден!<<<!!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    no_of_book_from_catalogue = value;
-                }
-            }
-        }
-        public string Genre { get { return genre; }
-            set
-            {
-                if (value == null)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("!!>>>Полето не може да остане празно!<<<!!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    genre = value;
-                }
-            }
-        }
-        public int No_Of_Personal_Card { get { return no_of_personal_card; }
+            get { return no_of_personal_card; }
             set
             {
                 if (value == null)
@@ -159,11 +39,13 @@
                 }
                 else
                 {
-                    no_of_book_from_catalogue = value;
+                    no_of_personal_card = value;
                 }
             }
         }
-        public string Names_Of_Reader { get { return names_of_reader; }
+        public string Names_Of_Reader
+        {
+            get { return names_of_reader; }
             set
             {
                 if (value == null)
@@ -175,11 +57,13 @@
                 }
                 else
                 {
-                    book_name = value;
+                    names_of_reader = value;
                 }
             }
         }
-        public long Egn { get { return egn; }
+        public long Egn
+        {
+            get { return egn; }
             set
             {
                 if (value == null)
@@ -202,7 +86,9 @@
                 }
             }
         }
-        public string Adress { get { return adress; }
+        public string Adress
+        {
+            get { return adress; }
             set
             {
                 if (value == null)
@@ -218,7 +104,9 @@
                 }
             }
         }
-        public string Phone_Number { get { return phone_number; }
+        public string Phone_Number
+        {
+            get { return phone_number; }
             set
             {
                 if (value == null)
@@ -234,7 +122,9 @@
                 }
             }
         }
-        public int Age_Of_Reader { get { return age_of_reader; }
+        public int Age_Of_Reader
+        {
+            get { return age_of_reader; }
             set
             {
                 if (value == null)
@@ -257,7 +147,9 @@
                 }
             }
         }
-        public string Date_Of_Creating_Personal_Card { get { return date_of_creating_personal_card; }
+        public string Date_Of_Creating_Personal_Card
+        {
+            get { return date_of_creating_personal_card; }
             set
             {
                 if (value == null)
@@ -273,7 +165,9 @@
                 }
             }
         }
-        public string Date_Of_Returning_The_Book { get { return date_of_returning_the_book; }
+        public string Date_Of_Returning_The_Book
+        {
+            get { return date_of_returning_the_book; }
             set
             {
                 if (value == null)
@@ -289,7 +183,9 @@
                 }
             }
         }
-        public int No_Of_Returned_Book_From_Catalogue { get { return no_of_returned_book_from_catalogue; }
+        public int No_Of_Returned_Book_From_Catalogue
+        {
+            get { return no_of_returned_book_from_catalogue; }
             set
             {
                 if (value == null)
@@ -312,7 +208,9 @@
                 }
             }
         }
-        public string Author_Of_Returned_Book { get { return author_of_returned_book; }
+        public string Author_Of_Returned_Book
+        {
+            get { return author_of_returned_book; }
             set
             {
                 if (value == null)
@@ -328,7 +226,9 @@
                 }
             }
         }
-        public string Name_Of_Returned_Book { get { return name_of_returned_book; }
+        public string Name_Of_Returned_Book
+        {
+            get { return name_of_returned_book; }
             set
             {
                 if (value == null)
@@ -345,14 +245,8 @@
             }
         }
 
-        public LibrarySystem(string book_name, string author, string publishing_house, int year_of_publishing, int no_of_book_from_catalogue, string genre, int no_of_personal_card, string names_of_reader, long egn, string adress, string phone_number, int age_of_reader, string date_of_creating_personal_card, string date_of_returning_the_book, int no_of_returned_book_from_catalogue, string author_of_returned_book, string name_of_returned_book)
+        public LibrarySystem_Reader (int no_of_personal_card, string names_of_reader, long egn, string adress, string phone_number, int age_of_reader, string date_of_creating_personal_card, string date_of_returning_the_book, int no_of_returned_book_from_catalogue, string author_of_returned_book, string name_of_returned_book)
         {
-            this.Book_Name = book_name;
-            this.Author = author;
-            this.Publishing_House = publishing_house;
-            this.Year_Of_Publishing = year_of_publishing;
-            this.No_Of_Book_From_Catalogue = no_of_book_from_catalogue;
-            this.Genre = genre;
             this.No_Of_Personal_Card = no_of_personal_card;
             this.Names_Of_Reader = names_of_reader;
             this.Egn = egn;
@@ -364,11 +258,6 @@
             this.No_Of_Returned_Book_From_Catalogue = no_of_returned_book_from_catalogue;
             this.Author_Of_Returned_Book = author_of_returned_book;
             this.Name_Of_Returned_Book = name_of_returned_book;
-        } 
-        //private int AverageAgeOfBooks(int age)
-        //{
-        //    age = 2026 - year_of_publishing;
-        //    return age;
-        //}
+        }
     }
 }
